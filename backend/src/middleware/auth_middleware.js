20863076f06e02e32f4075f320024686409e901a
chislain <adelininfo08@gmail.com>
2026-09-13 12:45:34 +0100

    Addition of authentication middleware

---
+++ backend/src/middleware/auth_middleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import AppError from '../errors/AppErrors.js';
+
+export function authenticateToken(req, res, next) {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        throw new AppError('Token d’authentification manquant', 401);
+    }
+
+    const [type, token] = authorization.split(' ');
+
+    if (type !== 'Bearer' || !token) {
+        throw new AppError('Format du token invalide', 401);
+    }
+
+    try {
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        throw new AppError('Token invalide ou expiré', 401);
+    }
+}
